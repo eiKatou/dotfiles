0c86437279f0f994dedb8b291a787d0aee1eb3b8
かとう えい <n17920517@gmail.com>
2020-11-24 11:46:29 +0900

    mod

--- bin/unmountTimeMachineDisk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-date>>/Users/ei/bin/mount.log
-echo "unmount">>/Users/ei/bin/mount.log
-/usr/sbin/diskutil unmount /Volumes/Time
-/usr/sbin/diskutil unmount /Volumes/VOL1
-/usr/sbin/diskutil unmount /Volumes/VOL2
