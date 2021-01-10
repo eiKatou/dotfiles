7c451c41b615130fe289e9ef0a3381b81ee3b3f1
かとう えい <n17920517@gmail.com>
2021-01-10 10:20:44 +0900

    修正

--- bin/smallvideo.sh
+++ bin/smallvideo.sh
@@ -2,22 +2,18 @@
 
 # smallvideo.sh inputfile
 
-# check arguments
-if [ $# -ne 1 ]; then
-  echo "usage: smallvideo.sh inputfile"
-  exit 1
-fi
+for i in "$@"; do
+  # アウトプットファイル名を決定
+  INPUT=$i
+  OUTPUT=small_$i
+  echo "$INPUT -> $OUTPUT"
 
-# アウトプットファイル名を決定
-INPUT=$1
-OUTPUT=small_$1
-echo "$INPUT -> $OUTPUT"
+  # ファイル作成日を取得
+  CREATEDATE=`GetFileInfo -d "$INPUT"`
 
-# ファイル作成日を取得
-CREATEDATE=`GetFileInfo -d  $INPUT`
+  # 動画縮小
+  HandbrakeCLI -i "$INPUT" -o "$OUTPUT" -X 720 -Y 720 -f mp4 -e x264 -r 30 --pfr
 
-# 動画縮小
-HandbrakeCLI -i $INPUT -o $OUTPUT -X 720 -Y 720 -f mp4 -e x264 -r 30 --pfr
-
-# ファイル作成日を設定
-SetFile -d "$CREATEDATE" $OUTPUT
+  # ファイル作成日を設定
+  SetFile -d "$CREATEDATE" "$OUTPUT"
+done
